821146c5185c944c9e5c7ff5689bcf3c3e152c40
Your Name <you@example.com>
2023-06-12 11:51:11 +0200

    modif 11h48

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -3,7 +3,7 @@ package com.m2i.tp;
 public class MyApp {
 
 	public static void main(String[] args) {
-		System.out.println("lundi 12 juin 2023");
+		System.out.println("lundi 12 juin 2023 11h48");
 	}
 
 }
