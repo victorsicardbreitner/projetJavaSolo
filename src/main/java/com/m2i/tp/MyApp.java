397f52a5d44f6b237ac11fefb935f4a95d5d7a66
victorsicardbreitner <81385307+victorsicardbreitner@users.noreply.github.com>
2023-06-12 18:28:18 +0200

    remise à zéro

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -5,10 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		System.out.println("lundi 12 juin 2023 12h14");
 		System.out.println("bientôt déjeuner");
-		System.out.println("devA is the best !!!");
-		System.out.println("devB is the very best !!!");
-		System.out.println("conflit devB");
-		System.out.println("Nouvelle tentative devA");
+		System.out.println("remise à zéro");
 	}
 
 }
